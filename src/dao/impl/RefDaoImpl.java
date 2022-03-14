7f1728a6797faddbf23d4ee5c7e90377a39e25fa
hlion17 <yourdad20626@gmail.com>
2022-03-15 08:50:08 +0900

    .

--- src/dao/impl/RefDaoImpl.java
+++ src/dao/impl/RefDaoImpl.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import common.JDBCTemplate;
 import dao.face.RefDao;
