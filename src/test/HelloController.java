b695b4b3bf977179755dbb63aa6ebbcf0d20c962
hlion17 <yourdad20626@gmail.com>
2022-03-14 19:34:47 +0900

    test commit

--- src/test/HelloController.java
+++ src/test/HelloController.java
@@ -4,11 +4,10 @@ package test;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-/**
- * Servlet implementation class HelloController
- */
 @WebServlet({ "/HelloController", "/hello" })
 public class HelloController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+		private void sysout() {
+			
+		}
 }
