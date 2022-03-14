be6d9088073e0432c49d222b11d9e0af41a4caa1
tiutni <tiutni.dev@gmail.com>
2022-03-14 19:39:17 +0900

    충돌상황유도

--- src/test/HelloController.java
+++ src/test/HelloController.java
@@ -1,8 +1,13 @@
 package test;
 
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class HelloController
@@ -11,4 +16,8 @@ import javax.servlet.http.HttpServlet;
 public class HelloController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			System.out.println("요요췌키라웃");
+		}
 }
