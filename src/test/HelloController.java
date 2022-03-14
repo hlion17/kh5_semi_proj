4654efbca02f50359817516706b108c018c75940
hlion17 <yourdad20626@gmail.com>
2022-03-14 19:36:52 +0900

    test commit2

--- src/test/HelloController.java
+++ src/test/HelloController.java
@@ -1,13 +1,14 @@
 package test;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 @WebServlet({ "/HelloController", "/hello" })
 public class HelloController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-		private void sysout() {
-			
-		}
+		List list = new ArrayList();
 }
