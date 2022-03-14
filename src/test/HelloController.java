97de9a4f0dcd2bec1a081a2711d3268e070f2ced
hlion17 <yourdad20626@gmail.com>
2022-03-14 20:03:26 +0900

    test

--- src/test/HelloController.java
+++ src/test/HelloController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet({ "/HelloController", "/hello" })
 public class HelloController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-		List list = new ArrayList();
+
 	@Override
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			System.out.println("요요췌키췌키");
