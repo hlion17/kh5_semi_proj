24648075ac490cbec995f8b033406b33500b6b06
hlion17 <yourdad20626@gmail.com>
2022-03-14 20:04:26 +0900

    test2

--- src/test/HelloController.java
+++ src/test/HelloController.java
@@ -1,9 +1,9 @@
 package test;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,5 +16,6 @@ public class HelloController extends HttpServlet {
 	@Override
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			System.out.println("요요췌키췌키");
+			System.out.println("테스트");
 		}
 }
