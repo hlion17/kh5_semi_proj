15d93b456844c4e49ca58efe5a353bb90fc9e19d
tiutni <tiutni.dev@gmail.com>
2022-03-15 17:09:42 +0900

    유저랭킹 작업중 - 컨트롤러

--- WebContent/WEB-INF/views/community/rank/rankMember.jsp
+++ WebContent/WEB-INF/views/community/rank/rankMember.jsp
@@ -4,9 +4,12 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>멤버 순위</title>
 </head>
 <body>
+
+<h1>멤버순위</h1>
+<hr>
 
 </body>
 </html>
--- src/controller/community/rank/RankMemberController.java
+++ src/controller/community/rank/RankMemberController.java
@@ -1,10 +1,24 @@
 package controller.community.rank;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/rank/member")
 public class RankMemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("[TEST] /rank/member - RankMemberController [GET] 호출");
+		
+		//JSP를 VIEW로 지정, View로 응답
+		System.out.println("[TEST] rankMember.jsp로 포워드");
+		req.getRequestDispatcher("/WEB-INF/views/community/rank/rankMember.jsp").forward(req, resp);
+	}
 
 }
