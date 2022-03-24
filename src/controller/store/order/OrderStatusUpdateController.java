1f0e9ffddd1dcbab958cc5268fd02c0a31f8a9aa
HYJeon <h_yeun12@naver.com>
2022-03-24 21:15:40 +0900

    메인 ui 수정완료

---
+++ src/controller/store/order/OrderStatusUpdateController.java
@@ -0,0 +1,27 @@
+package controller.store.order;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.face.OrderingService;
+import service.impl.OrderingServiceImpl;
+
+@WebServlet("/order/update")
+public class OrderStatusUpdateController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private OrderingService orderingService = new OrderingServiceImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 주문 상태 변경하는 서비스
+		orderingService.updateStatus(req);
+		
+		// 주문 확인 창으로 리다이렉트
+		resp.sendRedirect("/order/check");
+	}
+}
